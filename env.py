1ad5108617cb7818919949b9394d10cd148370e8
Neilfoster <Neilfoster81@gmail.com>
2020-01-30 11:34:00 +0000

    Added WhiteNoise to requirements.txt and settings.py

--- env.py
+++ env.py
@@ -1,3 +1,7 @@
 import os
 
-os.environ.setdefault("SECRET_KEY", "2#p!1-&n&=#1n3#la7vq*o$cnl&@kg_xsalx+!+uwu&px*z379")
+os.environ.setdefault("SECRET_KEY", 
+"'2#p!1-&n&=#1n3#la7vq*o$cnl&@kg_xsalx+!uwu&px*z379'")
+
+os.environ.setdefault("DATABASE_URL", 
+"postgres://hkcyqgwbnvfddw:384ec726f6281df00b25ec71eb3ea4e9c746e90e152999ed656eb46ff796ca43@ec2-54-247-170-5.eu-west-1.compute.amazonaws.com:5432/dbbvjur71f2a5q")
